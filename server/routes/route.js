63e0bec70d30b3b6713dde6ad08da298a04c6d6c
Harshdeep Khartade <138711003+harshdeepKhartade10@users.noreply.github.com>
2025-02-07 19:03:30 +0530

    Update route.js

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,4 +1,16 @@
 // server/routes/route.js
+// import express from "express";
+// import { getUploadUrlController, getViewUrlController } from "../controller/image-controller.js";
+
+// const router = express.Router();
+
+// router.get("/upload-url", getUploadUrlController);
+// router.get("/view-url", getViewUrlController);
+
+
+
+// export default router;
+
 import express from "express";
 import { getUploadUrlController, getViewUrlController } from "../controller/image-controller.js";
 
@@ -7,7 +19,13 @@ const router = express.Router();
 router.get("/upload-url", getUploadUrlController);
 router.get("/view-url", getViewUrlController);
 
+// Add this to handle /view-all (optional)
+router.get("/view-all", (req, res) => {
+  res.status(200).send("View All Page - This should be handled in frontend.");
+});
+
 export default router;
+
 // server/routes/route.js
 // import express from "express";
 // import { getUploadUrlController, getViewUrlController } from "../controller/image-controller.js";
